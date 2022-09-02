93ef4239eddb5a630797029910d2a9643ad3928f
RohanCarlo <69616556+RohanCarlo@users.noreply.github.com>
2022-09-02 19:00:39 +0530

    Update bucketer.c

--- bucketer.c
+++ bucketer.c
@@ -5,6 +5,11 @@ struct CountsByUsage {
   int lowCount;
   int mediumCount;
   int highCount;
+  int lowCycleArray[100];
+  int mediumCycleArray[100];
+  int highCycleArray[100];
+ 
+  
 };
 
 struct CountsByUsage countBatteriesByUsage(const int* cycles, int nBatteries) {
@@ -14,12 +19,41 @@ struct CountsByUsage countBatteriesByUsage(const int* cycles, int nBatteries) {
 
 void testBucketingByNumberOfCycles() {
   const int chargeCycleCounts[] = {100, 300, 500, 600, 900, 1000};
+  int a=0;
+ 
   const int numberOfBatteries = sizeof(chargeCycleCounts) / sizeof(chargeCycleCounts[0]);
   printf("Counting batteries by usage cycles...\n");
   struct CountsByUsage counts = countBatteriesByUsage(chargeCycleCounts, numberOfBatteries);
-  assert(counts.lowCount == 2);
-  assert(counts.mediumCount == 3);
-  assert(counts.highCount == 1);
+  for(int i=0; i<=; i++)
+  {
+    for(int j=0; j<numberOfBatteries; j++)
+    {
+      
+    if(chargeCycleCounts[i]<400)
+    {
+      counts.lowCycleArray[j]=chargeCycleCounts[i];       //adding the charge cycle count to an array storing low cycle counts for future reference
+      counts.lowCount++;                                  //incrementing lowCount if batterycycles is less than 400
+    }
+    if(chargeCycleCounts[i]>=400 && chargeCycleCounts[i]<=919)
+    {
+      counts.mediumCycleArray[j]=chargeCycleCounts[i];    //adding the charge cycle count to an array storing medium cycle counts for future reference
+      counts.mediumCount++;                               //incrementing mediumCount if batterycycles is between 400 and 919
+    }
+    if(chargeCycleCounts[i]>=920)
+    {
+      counts.highCycleArray[j]=chargeCycleCounts[i];      //adding the charge cycle count to an array storing high cycle counts for future reference
+      counts.highCount++;                                 //incrementing highCount if batterycycles is more than 920
+    }
+    
+    }
+}   
+       
+      
+
+  printf("Low cycle count = %d\n",counts.lowCount);
+  printf("Medium cycle count = %d\n",counts.mediumCount);
+  printf("High cycle count = %d\n",counts.highCount);
+  
   printf("Done counting :)\n");
 }
 
